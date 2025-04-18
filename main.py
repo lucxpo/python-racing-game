966823e92c55207b44211cbff81762aedc6e8b95
lucxpo <187343698+lucxpo@users.noreply.github.com>
2025-04-18 12:21:01 -0300

    Change assets and display Player on screen

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from os.path import join
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
-        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.sprite_sheet = pygame.image.load(join("Assets", "Images", "player-Sheet.png")).convert_alpha()
+        self.frame_rect = (0, 0, 32, 32)
+        self.image = self.sprite_sheet.subsurface(self.frame_rect).copy()
         self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
 
 # general setup
 pygame.init()
@@ -14,6 +17,9 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Racing Mania")
 clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+player = Player(all_sprites)
 
 # game loop
 running = True
@@ -26,5 +32,7 @@ while running:
 
     # draw
     display_surface.fill((20, 126, 20))
+
+    all_sprites.draw(display_surface)
 
     pygame.display.update()
